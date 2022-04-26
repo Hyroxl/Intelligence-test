5d09cc97bcba35ba5b904db1499c4b3e6d9ffd49
Tyler Paul <98190955+Hyroxl@users.noreply.github.com>
2022-04-25 20:39:47 -0400

    Create intelligence-test.c

---
+++ intelligence-test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+int main()
+{
+    char name[100];
+    printf("whats their name: \n");
+    scanf("%s", name);
+    srand(clock());
+    int randomnum = rand()%101;
+    int trufal = 2;
+    if (randomnum >= 50) {
+        trufal = 1;
+    } else {
+        trufal = 0;
+    }
+    if (trufal == 1){
+    printf("their score is %d \n", randomnum);
+    printf("%s %s \n", name, " is smart");
+    } else if (trufal == 0) {
+    printf("their score is %d \n", randomnum);
+    printf("%s %s \n", name, " is stupid");
+    }
+}
