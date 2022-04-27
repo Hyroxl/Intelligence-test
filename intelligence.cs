66b35e05841aadf173ae547c32da80f8bcaf6074
Hyroxl <98190955+Hyroxl@users.noreply.github.com>
2022-04-27 16:12:47 -0400

    Update intelligence.cs

--- intelligence.cs
+++ intelligence.cs
@@ -10,7 +10,7 @@ namespace Intelligence
         int randomnum2 = rnd.Next(0, 5);
         bool smart = false;
         Console.WriteLine("whats their name?:");
-        String name = "test";
+        String name = Console.ReadLine();
         if (randomnum >= 50) {
         smart = true;
         }else if (randomnum < 50) {
@@ -20,22 +20,22 @@ namespace Intelligence
         Console.WriteLine("their score is " + randomnum);
         Console.WriteLine(name + " is smart");
         }else if (smart == false) {
-        if (randomnum2 == 0){
-        	Console.WriteLine("their score is " + randomnum);
-        	Console.WriteLine(name + " is stupid");
-        } else if (randomnum2 == 1) {
-        	Console.WriteLine("their score is " + randomnum);
-        	Console.WriteLine(name + " is maidenless");
-        } else if (randomnum2 == 2) {
-        	Console.WriteLine("their score is " + randomnum);
-        	Console.WriteLine(name + " needs to touch grass");
-        } else if (randomnum2 == 3) {
-        	Console.WriteLine("their score is " + randomnum);
-        	Console.WriteLine("your mom");
-        } else id (randomnum2 == 4) {
-        	Console.WriteLine("their score is " + randomnum);
-        	Console.WriteLine(name + " is short");
-        }
+          if (randomnum2 == 0){
+              Console.WriteLine("their score is " + randomnum);
+              Console.WriteLine(name + " is stupid");
+          } else if (randomnum2 == 1) {
+              Console.WriteLine("their score is " + randomnum);
+              Console.WriteLine(name + " is maidenless");
+          } else if (randomnum2 == 2) {
+              Console.WriteLine("their score is " + randomnum);
+              Console.WriteLine(name + " needs to touch grass");
+          } else if (randomnum2 == 3) {
+              Console.WriteLine("their score is " + randomnum);
+              Console.WriteLine("your mom");
+          } else if (randomnum2 == 4) {
+              Console.WriteLine("their score is " + randomnum);
+        	  Console.WriteLine(name + " is short");
+          }
         }
     }
   }
