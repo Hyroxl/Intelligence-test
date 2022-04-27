ee9ed5007d88f080fcc4c922cb1a217b5c694b27
Hyroxl <98190955+Hyroxl@users.noreply.github.com>
2022-04-27 11:20:18 -0400

    Update intelligence.c

--- intelligence.c
+++ intelligence.c
@@ -8,6 +8,7 @@ int main()
     printf("whats their name: \n");
     scanf("%s", name);
     srand(clock());
+    int randomnum2 = rand()%5;
     int randomnum = rand()%101;
     int trufal = 2;
     if (randomnum >= 50) {
@@ -19,8 +20,22 @@ int main()
     printf("their score is %d \n", randomnum);
     printf("%s %s \n", name, " is smart");
     } else if (trufal == 0) {
-    printf("their score is %d \n", randomnum);
-    printf("%s %s \n", name, " is stupid");
+        if (randomnum2 == 0) {
+        printf("their score is %d \n", randomnum);
+        printf("%s %s \n", name, " is stupid");
+        } else if (randomnum2 == 1) {
+        printf("their score is %d \n", randomnum);
+        printf("%s %s \n", name, " is maidenless");
+        } else if (randomnum2 == 2) {
+        printf("their score is %d \n", randomnum);
+        printf("%s %s \n", name, " needs to touch grass");
+        } else if (randomnum2 == 3) {
+        printf("their score is %d \n", randomnum);
+        printf("%s \n", "your mom");
+        } else if (randomnum2 == 4) {
+        printf("their score is %d \n", randomnum);
+        printf("%s %s \n", name, " is short");
+        }
     }
     return 0;
 }
