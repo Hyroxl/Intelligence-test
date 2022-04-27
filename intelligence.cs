e57735b5714e1f47117aed7b71ffe7f24c45fc91
Hyroxl <98190955+Hyroxl@users.noreply.github.com>
2022-04-27 14:15:15 -0400

    Update intelligence.cs

--- intelligence.cs
+++ intelligence.cs
@@ -7,9 +7,10 @@ namespace Intelligence
     {
     	var rnd = new Random();
         int randomnum = rnd.Next(0, 101);
+        int randomnum2 = rnd.Next(0, 5);
         bool smart = false;
         Console.WriteLine("whats their name?:");
-        String name = Console.ReadLine();
+        String name = "test";
         if (randomnum >= 50) {
         smart = true;
         }else if (randomnum < 50) {
@@ -19,8 +20,22 @@ namespace Intelligence
         Console.WriteLine("their score is " + randomnum);
         Console.WriteLine(name + " is smart");
         }else if (smart == false) {
-        Console.WriteLine("their score is " + randomnum);
-        Console.WriteLine(name + " is stupid");
+        if (randomnum2 == 0){
+        	Console.WriteLine("their score is " + randomnum);
+        	Console.WriteLine(name + " is stupid");
+        } else if (randomnum2 == 1) {
+        	Console.WriteLine("their score is " + randomnum);
+        	Console.WriteLine(name + " is maidenless");
+        } else if (randomnum2 == 2) {
+        	Console.WriteLine("their score is " + randomnum);
+        	Console.WriteLine(name + " needs to touch grass");
+        } else if (randomnum2 == 3) {
+        	Console.WriteLine("their score is " + randomnum);
+        	Console.WriteLine("your mom");
+        } else id (randomnum2 == 4) {
+        	Console.WriteLine("their score is " + randomnum);
+        	Console.WriteLine(name + " is short");
+        }
         }
     }
   }
