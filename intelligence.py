b75a27bc29ffffd84d55af7e63b8172b2ed8a331
Hyroxl <98190955+Hyroxl@users.noreply.github.com>
2022-04-27 13:56:34 -0400

    Update intelligence.py

--- intelligence.py
+++ intelligence.py
@@ -1,6 +1,7 @@
 from random import random
 import string
 name = input("whats your name?:")
+randomnum2 = int(random()* 5)
 randomnum = int(random()* 101)
 smart = "null"
 if randomnum >= 50:
@@ -11,5 +12,18 @@ if smart == True:
     print("their score is " + str(randomnum))
     print(name + " is smart")
 if smart == False:
-    print("their score is " + str(randomnum))
-    print(name + " is stupid")
+    if randomnum2 == 0:
+        print("their score is " + str(randomnum))
+        print(name + " is stupid")
+    if randomnum2 == 1:
+        print("their score is " + str(randomnum))
+        print(name + " is maidenless")
+    if randomnum2 == 2:
+        print("their score is " + str(randomnum))
+        print(name + " needs to touch grass")
+    if randomnum2 ==3:
+        print("their score is " + str(randomnum))
+        print("your mom")
+    if randomnum2 ==4:
+        print("their score is " + str(randomnum))
+        print(name + " is short")
